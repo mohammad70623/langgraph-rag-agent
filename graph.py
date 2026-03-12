6dfad486a2ab79c937edc600fd5c9b9bb22fce3a
Mohammad <mohammad70623@gmail.com>
2026-03-12 19:49:34 +0600

    build LangGraph workflow

--- graph.py
+++ graph.py
@@ -98,3 +98,13 @@ def chat_node(state: ChatState):
     return {"messages": [response]}
 
 tool_node = ToolNode(tools)
+
+# Graph
+graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_node("tools", tool_node)
+graph.add_edge(START, "chat_node")
+graph.add_conditional_edges("chat_node", tools_condition)
+graph.add_edge("tools", "chat_node")
+
+chatbot = graph.compile()
